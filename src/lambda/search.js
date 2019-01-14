d3f358ca11c2fad981b73d8a80313d350bfabb91
Roger Zurawicki <roger@zura.wiki>
2019-01-14 18:21:44 +0800

    Add GeoIP database

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -1,9 +1,20 @@
 const isChinese = require('is-chinese')
+const maxmind = require('maxmind');
+const path = require('path');
 const { stringify } = require("qs");
 
 const BAIDU_ENGINE = ['https://www.baidu.com/s', 'wd']
 const BING_ENGINE = ['https://cn.bing.com/search', 'q']
 const GOOGLE_ENGINE = ['https://www.google.com/search', 'q']
+
+const GEOIP_DB = path.join('data', 'GeoLite2-Country.mmdb');
+
+// To debug your problem
+console.log(path.resolve(GEOIP_DB));
+
+
+/** Memoize loading of geoIP library */
+let countryLookup = maxmind.openSync(GEOIP_DB);
 
 function getEngine(searchQuery, ipAddress) {
   if (isChinese(searchQuery)) {
@@ -12,6 +23,8 @@ function getEngine(searchQuery, ipAddress) {
   }
 
   // ip is China
+  const country = ipAddress ? countryLookup.get(ipAddress) : null;
+  console.log(ipAddress, country)
   if (false) {
     return BING_ENGINE;
   }
@@ -19,24 +32,22 @@ function getEngine(searchQuery, ipAddress) {
   return GOOGLE_ENGINE;
 }
 
-function redirectToEngine([url, queryParamKey], query, callback) {
-  callback(null, {
+function redirectToEngine([url, queryParamKey], query) {
+  return {
     statusCode: 302,
     headers: {
       'Location': url + stringify({ [queryParamKey]: query }, { addQueryPrefix: true }),
     },
     body: '',
-  });
+  };
 }
 
-exports.handler = (event, context, callback) => {
-  console.log(event, context);
 
-  const { queryStringParameters } = event;
+exports.handler = async (event, context) => {
+  const { queryStringParameters = {}, headers = {} } = event;
   const query = queryStringParameters['q'];
+  const ip = headers['client-ip']
+  const engine = getEngine(query, ip)
 
-  const engine = getEngine(query, '')
-
-  redirectToEngine(engine, query, callback)
+  return redirectToEngine(engine, query)
 };
-
