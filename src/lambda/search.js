bd790b9aa9cb4312626bb62a19aefc88e3b79666
Roger Zurawicki <roger@zura.wiki>
2019-01-14 20:10:17 +0800

    Deps upgrade

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -1,36 +1,41 @@
 const isChinese = require('is-chinese')
-const maxmind = require('maxmind');
 const fs = require('fs');
-const geolite2 = require('geolite2');
 const path = require('path');
 const { stringify } = require("qs");
+const MMDBReader = require('mmdb-reader');
+
 
 const BAIDU_ENGINE = ['https://www.baidu.com/s', 'wd']
 const BING_ENGINE = ['https://cn.bing.com/search', 'q']
 const GOOGLE_ENGINE = ['https://www.google.com/search', 'q']
 
-const GEOIP_DB = geolite2.paths.country;
+const dbBuffer = fs.readFileSync(path.join('data', 'GeoLite2-Country.mmdb'));
+// Load synchronously
+const countryLookup = new MMDBReader(dbBuffer);
+
 
 /** Memoize loading of geoIP library */
 // let countryLookup = maxmind.openSync(GEOIP_DB);
-let countryLookup = { get: () => null };
+// let countryLookup = { lookup: () => null };
+
+console.log(countryLookup.lookup('8.8.8.8'))
 
 function getEngine(searchQuery, ipAddress) {
 
-  // To debug your problem
-  console.log('TO DEBUG PROBLEM START');
-  console.log(path.resolve(GEOIP_DB));
-  console.log('TO DEBUG PROBLEM START 2');
-  console.log('..', path.resolve('..'), fs.readdirSync('..'));
-  console.log('TO DEBUG PROBLEM START 3');
-  console.log('/', path.resolve('/'), fs.readdirSync('/'));
-  console.log('__dirname', path.resolve(__dirname), fs.readdirSync(__dirname));
+  // // To debug your problem
+  // console.log('TO DEBUG PROBLEM START');
+  // console.log(path.resolve(GEOIP_DB));
+  // console.log('TO DEBUG PROBLEM START 2');
+  // console.log('..', path.resolve('..'), fs.readdirSync('..'));
+  // console.log('TO DEBUG PROBLEM START 3');
+  // console.log('/', path.resolve('/'), fs.readdirSync('/'));
+  // console.log('__dirname', path.resolve(__dirname), fs.readdirSync(__dirname));
 
-  console.log('TO DEBUG PROBLEM START 4');
-  console.log('.', path.resolve('.'), fs.readdirSync('.'));
-  console.log('TO DEBUG PROBLEM START 5');
-  console.log(fs.readdirSync('data'));
-  console.log('TO DEBUG PROBLEM END');
+  // console.log('TO DEBUG PROBLEM START 4');
+  // console.log('.', path.resolve('.'), fs.readdirSync('.'));
+  // console.log('TO DEBUG PROBLEM START 5');
+  // console.log(fs.readdirSync('data'));
+  // console.log('TO DEBUG PROBLEM END');
 
 
   if (isChinese(searchQuery)) {
@@ -39,7 +44,7 @@ function getEngine(searchQuery, ipAddress) {
   }
 
   // ip is China
-  const country = ipAddress ? countryLookup.get(ipAddress) : null;
+  const country = ipAddress ? countryLookup.lookup(ipAddress) : null;
   console.log(ipAddress, country)
   if (country && country.country && country.country.iso_code && country.country.iso_code === 'CN') {
     return BING_ENGINE;
