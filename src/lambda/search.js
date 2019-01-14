870e2b207813cc996a18c8eecbd9342791e23093
Roger Zurawicki <roger@zura.wiki>
2019-01-14 18:30:09 +0800

    more logs

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -9,23 +9,30 @@ const GOOGLE_ENGINE = ['https://www.google.com/search', 'q']
 
 const GEOIP_DB = path.join('data', 'GeoLite2-Country.mmdb');
 
-// To debug your problem
-console.log(path.resolve(GEOIP_DB));
+
 
 
 /** Memoize loading of geoIP library */
-let countryLookup = maxmind.openSync(GEOIP_DB);
+// let countryLookup = maxmind.openSync(GEOIP_DB);
+let countryLookup = null;
 
 function getEngine(searchQuery, ipAddress) {
+
+  // To debug your problem
+  console.log('TO DEBUG PROBLEM START');
+  console.log(path.resolve(GEOIP_DB));
+  console.log('TO DEBUG PROBLEM END');
+
+
   if (isChinese(searchQuery)) {
     // query is all Chinese characters
     return BAIDU_ENGINE;
   }
 
   // ip is China
-  const country = ipAddress ? countryLookup.get(ipAddress) : null;
+  const country = countryLookup && ipAddress ? countryLookup.get(ipAddress) : null;
   console.log(ipAddress, country)
-  if (false) {
+  if (country && country.country && country.country.iso_code && country.country.iso_code === 'CN') {
     return BING_ENGINE;
   }
 
