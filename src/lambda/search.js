d068a9350ab9a2eaa478704be8279d2633f0780d
Roger Zurawicki <roger@zura.wiki>
2019-01-14 19:10:16 +0800

    Add geoip package

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -1,5 +1,7 @@
 const isChinese = require('is-chinese')
 const maxmind = require('maxmind');
+const fs = require('fs');
+const geolite2 = require('geolite2');
 const path = require('path');
 const { stringify } = require("qs");
 
@@ -7,7 +9,7 @@ const BAIDU_ENGINE = ['https://www.baidu.com/s', 'wd']
 const BING_ENGINE = ['https://cn.bing.com/search', 'q']
 const GOOGLE_ENGINE = ['https://www.google.com/search', 'q']
 
-const GEOIP_DB = path.join('data', 'GeoLite2-Country.mmdb');
+const GEOIP_DB = geolite2.paths.country;
 
 /** Memoize loading of geoIP library */
 // let countryLookup = maxmind.openSync(GEOIP_DB);
@@ -19,8 +21,14 @@ function getEngine(searchQuery, ipAddress) {
   console.log('TO DEBUG PROBLEM START');
   console.log(path.resolve(GEOIP_DB));
   console.log('TO DEBUG PROBLEM START 2');
-  console.log(fs.readdirSync('.'));
+  console.log('..', path.resolve('..'), fs.readdirSync('..'));
   console.log('TO DEBUG PROBLEM START 3');
+  console.log('/', path.resolve('/'), fs.readdirSync('/'));
+  console.log('__dirname', path.resolve(__dirname), fs.readdirSync(__dirname));
+
+  console.log('TO DEBUG PROBLEM START 4');
+  console.log('.', path.resolve('.'), fs.readdirSync('.'));
+  console.log('TO DEBUG PROBLEM START 5');
   console.log(fs.readdirSync('data'));
   console.log('TO DEBUG PROBLEM END');
 
