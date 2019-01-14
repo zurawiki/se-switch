49e838fc934181d2b6452c22f58fc6fc1d7d01f8
Roger Zurawicki <roger@zura.wiki>
2019-01-14 20:41:35 +0800

    Cleanup unused dependencies

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -1,43 +1,15 @@
 const isChinese = require('is-chinese')
-const fs = require('fs');
-const path = require('path');
 const { stringify } = require("qs");
 const MMDBReader = require('mmdb-reader');
-
 
 const BAIDU_ENGINE = ['https://www.baidu.com/s', 'wd']
 const BING_ENGINE = ['https://cn.bing.com/search', 'q']
 const GOOGLE_ENGINE = ['https://www.google.com/search', 'q']
 
 const dbBuffer = require('buffer-loader!../../data/GeoLite2-Country.mmdb');
-// Load synchronously
 const countryLookup = new MMDBReader(dbBuffer);
 
-
-/** Memoize loading of geoIP library */
-// let countryLookup = maxmind.openSync(GEOIP_DB);
-// let countryLookup = { lookup: () => null };
-
-console.log(countryLookup.lookup('8.8.8.8'))
-
 function getEngine(searchQuery, ipAddress) {
-
-  // // To debug your problem
-  // console.log('TO DEBUG PROBLEM START');
-  // console.log(path.resolve(GEOIP_DB));
-  // console.log('TO DEBUG PROBLEM START 2');
-  // console.log('..', path.resolve('..'), fs.readdirSync('..'));
-  // console.log('TO DEBUG PROBLEM START 3');
-  // console.log('/', path.resolve('/'), fs.readdirSync('/'));
-  // console.log('__dirname', path.resolve(__dirname), fs.readdirSync(__dirname));
-
-  // console.log('TO DEBUG PROBLEM START 4');
-  // console.log('.', path.resolve('.'), fs.readdirSync('.'));
-  // console.log('TO DEBUG PROBLEM START 5');
-  // console.log(fs.readdirSync('data'));
-  // console.log('TO DEBUG PROBLEM END');
-
-
   if (isChinese(searchQuery)) {
     // query is all Chinese characters
     return BAIDU_ENGINE;
